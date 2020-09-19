1c85ee538cc645de4dc6b7504acd8a39a8e65adf
karad1818 <52450149+karad1818@users.noreply.github.com>
2020-09-19 16:51:45 +0530

    Create Day15.cpp

---
+++ Day15.cpp
@@ -0,0 +1,27 @@
+/* OOP : Object Oriented Programming
+      Video : https://www.youtube.com/watch?v=PFmuCDHHpwk
+      
+      
+--> It's paradigm of programming based on Objects rather than functions
+--> Pillars of OOP : 1.Encapsulation
+                     2.Abstraction
+                     3.Polymorphism
+                     4.Inheritance
+--> Problem in procedural programming : function dependency on too many functions that create too messy code.
+    Here is solution : we bind all interrelated functions(Methods) and variable (Property) into one box  (This box is know as class (Object)) <-- this is known as encapsulation
+    
+--> Abstraction : Only Provide info. that are usefull (e.g. switch of fan )
+--> Inherirance : eliminate redundent code 
+--> Polymorphism : Poly (Many) + Morph(Forms)
+
+Benefits : --> Encapsulation --> reduce complexity + increase reusability
+               Polymorphism --> refactor switch case 
+               Abstraction --> reduce complexity
+               Inheritance --> eliminate redundant code
+               
+
+ --> Object is refenced type in js
+ 
+
+*/
+
