d356f4732066455d70e7af4f56b107f9bbb3b11e
karad1818 <52450149+karad1818@users.noreply.github.com>
2020-09-20 23:28:47 +0530

    Create Day16.cpp

---
+++ Day16.cpp
@@ -0,0 +1,28 @@
+/* Today i've solve some of the previous questions from codechef cookoff */
+/* 
+https://www.codechef.com/problems/TABLET
+solution: https://www.codechef.com/viewsolution/38027460
+
+https://www.codechef.com/problems/UCL
+solution: https://www.codechef.com/viewsolution/38030195
+
+https://www.codechef.com/problems/CFMM
+solution: https://www.codechef.com/viewsolution/38032788
+
+https://www.codechef.com/problems/HUNGALGO
+solution: https://www.codechef.com/viewsolution/38032926
+
+https://www.codechef.com/problems/REACTION
+solution: https://www.codechef.com/viewsolution/38039212
+
+https://www.codechef.com/problems/GCDSET
+solution: https://www.codechef.com/viewsolution/38039756
+
+https://www.codechef.com/problems/PLAYSTR
+solution: https://www.codechef.com/viewsolution/38040420
+
+https://www.codechef.com/problems/NBONACCI
+solution: https://www.codechef.com/viewsolution/38029239
+
+
+*/
